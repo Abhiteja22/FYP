9f1ae16a81ec2d8839a61fc75977dc8f58b44efc
Abhiteja22 <66022649+Abhiteja22@users.noreply.github.com>
2023-10-19 15:58:47 +0800

    Display of data tutorial

--- fyp/app/urls.py
+++ fyp/app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('app/', views.app, name='app'),
     path('stocks/', views.stocks, name='stocks'),
     path('stocks/details/<int:id>', views.details, name='details')
--- fyp/app/views.py
+++ fyp/app/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import Stock
 
 # Create your views here.
+
+def main(request):
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
 
 def app(request):
     template = loader.get_template('myfirst.html')
